87de376a0edee8c75e5c7da53342a88d89aa195b
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-06-13 08:35:02 +0100

    Improved function name

--- test_csr_decoder.py
+++ test_csr_decoder.py
@@ -6,9 +6,9 @@ from csr import CSR
 
 class TestCsrDecoding(unittest.TestCase):
     def setUp(self):
-        self.ec_csr = get_csr_from_file('ec-csr.pem')
-        self.dsa_csr = get_csr_from_file('dsa-csr.pem')
-        self.rsa_csr = get_csr_from_file('rsa-csr.pem')
+        self.ec_csr = get_csr_from_pem_file('ec-csr.pem')
+        self.dsa_csr = get_csr_from_pem_file('dsa-csr.pem')
+        self.rsa_csr = get_csr_from_pem_file('rsa-csr.pem')
 
     def test_ec_key_alg(self):
         self.assertEqual(self.ec_csr.get_pubkey_alg(), 'ECDSA')
@@ -50,7 +50,7 @@ class TestCsrDecoding(unittest.TestCase):
         self.assertEqual(self.rsa_csr.cn, b'rkc.com')
 
 
-def get_csr_from_file(filename):
+def get_csr_from_pem_file(filename):
     dir_name = os.path.dirname(os.path.realpath(__file__))
     fname = filename
     fname_full = os.path.join(dir_name, fname)
@@ -58,7 +58,6 @@ def get_csr_from_file(filename):
         pem = f.read()
     csr = CSR.from_pem(pem)
     return csr
-
 
 if __name__ == '__main__':
     log_level = logging.ERROR
